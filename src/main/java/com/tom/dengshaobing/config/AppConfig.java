4d4f6d1489ed129156ef41cdab32e218149cdfba
Tommy Deng <250575979@qq.com>
2016-11-22 23:36:58 +0800

    sa

--- src/main/java/com/tom/dengshaobing/config/AppConfig.java
+++ src/main/java/com/tom/dengshaobing/config/AppConfig.java
@@ -4,11 +4,8 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
