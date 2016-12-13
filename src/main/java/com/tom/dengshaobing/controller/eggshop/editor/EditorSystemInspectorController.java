1bbf92ebea5db65802a1df3d7ab0a23dd45fdeb0
Tommy Deng <250575979@qq.com>
2016-12-13 22:12:38 +0800

    sa

--- src/main/java/com/tom/dengshaobing/controller/eggshop/editor/EditorSystemInspectorController.java
+++ src/main/java/com/tom/dengshaobing/controller/eggshop/editor/EditorSystemInspectorController.java
@@ -1,7 +1,16 @@
 package com.tom.dengshaobing.controller.eggshop.editor;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,14 +34,27 @@ public class EditorSystemInspectorController extends BaseController {
 
 	@Autowired
 	private Environment env;
-	
+
+	@Autowired
+	private MessageSource messageSource;
+
 	@RequestMapping("/systemInspector")
 	public String systemInspector(@RequestParam(name = "visitId", required = false) String visitId,
 			@RequestParam(name = "visitType", required = false) String visitType, ModelMap map, String AT)
 			throws Exception {
 		AT = pageInit(AT, visitId, visitType, map);
-		
 
+		Map<String, Object> propertyMap = new TreeMap<>();
+		for (Iterator<PropertySource<?>> it = ((AbstractEnvironment) env).getPropertySources().iterator(); it
+				.hasNext();) {
+			PropertySource<?> propertySource = it.next();
+			if (propertySource instanceof ResourcePropertySource) {
+				propertyMap.putAll(((MapPropertySource) propertySource).getSource());
+			}
+		}
+		
+		map.put("propertyMap", propertyMap);
+		
 		return BasePath + "systemInspector";
 	}
 
